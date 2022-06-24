941e1f96b681a63c406a180614e3ad49d851dd0c
baserohit <mehrarohit996@gmail.com>
2022-06-24 09:21:22 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,41 @@
+// Alanog Clock
+setInterval(() => {
+  d = new Date(); //object of date()
+  hr = d.getHours();
+  min = d.getMinutes();
+  sec = d.getSeconds();
+  hr_rotation = 30 * hr + min / 2; //converting current time
+  min_rotation = 6 * min;
+  sec_rotation = 6 * sec;
+
+  hour.style.transform = `rotate(${hr_rotation}deg)`;
+  minute.style.transform = `rotate(${min_rotation}deg)`;
+  second.style.transform = `rotate(${sec_rotation}deg)`;
+
+  //   Digital Clock
+  let hours = document.getElementById("hours");
+  let minutes = document.getElementById("minutes");
+  let seconds = document.getElementById("seconds");
+  let ampm = document.getElementById("ampm");
+
+  let h = new Date().getHours();
+  let m = new Date().getMinutes();
+  let s = new Date().getSeconds();
+
+  let am = h >= 12? "PM" : "AM";
+
+  //   Convert 24hr clock to 12hr clock
+  if (h > 12){
+      h = h - 12;
+  }
+
+  // Add Zero before Single Digits
+  h = h < 10 ? "0" + h : h;
+  m = m < 10 ? "0" + m : m;
+  s = s < 10 ? "0" + s : s;
+
+  hours.innerHTML = h;
+  minutes.innerHTML = m;
+  seconds.innerHTML = s;
+  ampm.innerHTML = am;
+});
